2a844b31d28fb4ee3900072781fae0139797face
Sajjad Khan <mdsajjad9334578745@gmail.com>
2026-03-31 18:52:49 +0530

    fix footer issue

--- footer.js
+++ footer.js
@@ -2,15 +2,15 @@ const footerHTML = `
   <footer class="section-footer">
     <div class="footer-container container">
       <div class="content_1">
-        <img src="./public/images/nav-bar.png" alt="logo" />
+        <img src="/images/nav-bar.png" alt="logo" />
         <p>
           Welcome to Apna EcomStore, <br />
-          your ultimate destination for
-          <br />
+          your ultimate destination for <br />
           cutting-edge gadgets!
         </p>
         <img src="https://i.postimg.cc/Nj9dgJ98/cards.png" alt="cards" />
       </div>
+
       <div class="content_2">
         <h4>SHOPPING</h4>
         <a href="#">Computer Store</a>
@@ -18,33 +18,39 @@ const footerHTML = `
         <a href="#">Accessories</a>
         <a href="#">Sales & Discount</a>
       </div>
+
       <div class="content_3">
         <h4>Experience</h4>
-        <a href="./contact.html">Contact Us</a>
-        <a href="" target="_blank"> Payment Method </a>
-        <a href="" target="_blank"> Delivery </a>
-        <a href="" target="_blank"> Return and Exchange </a>
+        <a href="/contact.html">Contact Us</a>
+        <a href="#">Payment Method</a>
+        <a href="#">Delivery</a>
+        <a href="#">Return and Exchange</a>
       </div>
+
       <div class="content_4">
         <h4>NEWSLETTER</h4>
         <p>
-          Be the first to know about new
-          <br />
+          Be the first to know about new <br />
           arrivals, sales & promos!
         </p>
         <div class="f-mail">
           <input type="email" placeholder="Your Email" />
-          <i class="bx bx-envelope"></i>
         </div>
         <hr />
       </div>
     </div>
+
     <div class="f-design">
       <div class="f-design-txt">
         <p>Design and Code by MD SAJJAD & MD AMJAD ALI</p>
       </div>
     </div>
-  </footer>`;
+  </footer>
+`;
 
-const footerElem = document.querySelector(".section-footer");
-footerElem.insertAdjacentHTML("afterbegin", footerHTML);
+// ✅ correct selector
+const footerElem = document.querySelector("#footer");
+
+if (footerElem) {
+  footerElem.insertAdjacentHTML("afterbegin", footerHTML);
+}
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import products from "./api/products.json";
 import { showProductContainer } from "./homeProductCards";
+import "./footer.js";
 
 // Define a function named `showProductContainer` that takes an array of products as input.
 showProductContainer(products);
